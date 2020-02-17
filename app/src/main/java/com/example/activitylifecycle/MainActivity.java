9a49a8406acb9b17d74e0b7ba2518e1c56a9e6f8
ivanetra <ivanekaputra24@gmail.com>
2020-02-17 10:42:29 +0700

    Add toast to onCreate method

--- app/src/main/java/com/example/activitylifecycle/MainActivity.java
+++ app/src/main/java/com/example/activitylifecycle/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.activitylifecycle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("lifecycle", "onCreated invoked");
+        Toast.makeText(this, "onCreated invoked", Toast.LENGTH_SHORT).show();
     }
 }
